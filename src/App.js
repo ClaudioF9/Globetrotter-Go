f36380e937db4b8872490b5acdf4606025f67e81
ozgencmardi <ozgencmardi@gmail.com>
2023-03-22 19:42:02 +0000

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
 
 function App() {
   return <div>hello</div>
 }
+
+  <div className="App">
+    <Contact />
+    <Footer />
+  </div>
 
 
 export default App;
