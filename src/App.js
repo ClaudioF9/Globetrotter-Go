273409d5ea6c8aefc18086dfe4e58b5026c6eebc
ozgencmardi <ozgencmardi@gmail.com>
2023-03-23 18:32:48 +0000

    Contact Section Added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React from 'react';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
-  return <div>hello</div>
+  return (
+    <div>
+      <div>hello</div>
+      <Contact />
+      <Footer />
+    </div>
+  );
 }
-
 
 export default App;
