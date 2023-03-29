a0bed5b0f1436b0fa69797fe4185c62ff98e70a9
JC6991 <118604790+JC6991@users.noreply.github.com>
2023-03-29 07:46:00 +0100

    Update suggestions.js

--- src/pages/suggestions/suggestions.js
+++ src/pages/suggestions/suggestions.js
@@ -38,31 +38,3 @@ function Suggestions({ cheapFlights }) {
 
 export default Suggestions;
 
-
-
-
-// <Grid container spacing={0} columnSpacing={0} className="gridFormat">
-//     {flightInfo.origin_to_destination_trip.map((flightArray) =>
-//         flightArray.map((flightObj) => (
-//             <Grid item key={flightObj.flight_number} xs={12} sm={6} md={3}>
-//                 <Card className="cardy" sx={{ borderRadius: 8 }}>
-//                     <CardContent>
-//                         <Typography variant="h4" component="h2">
-//                             {flightObj.flight_number}
-//                         </Typography>
-//                         <Typography color="text.secondary">
-//                             {flightObj.origin_code} to {flightObj.destination_code}
-//                         </Typography>
-//                         <Typography variant="body2">
-//                             <p>Departure time: {flightObj.departure_datetime_utc}</p>
-//                             <p>Arrival time: {flightObj.arrival_datetime_utc}</p>
-//                             <p>
-//                                 Fare: {flightObj.regular_fare} {flightObj.currency}
-//                             </p>
-//                         </Typography>
-//                     </CardContent>
-//                 </Card>
-//             </Grid>
-//         ))
-//     )}
-// </Grid>
