1ec4ed6ce9fdbe88ec3c8a74200cba37418ff019
ClaudioF9 <119876939+ClaudioF9@users.noreply.github.com>
2023-03-30 17:52:49 +0100

    Update cardapi.js

--- src/components/cardapi/cardapi.js
+++ src/components/cardapi/cardapi.js
@@ -24,7 +24,7 @@ function FlightApi(props) {
         destination_departure_date: valueTwo ? valueTwo.format("YYYY-MM-DD") : null
       },
       headers: {
-        'X-RapidAPI-Key': 'f52fefffa7msh4adaf3573b9c473p12a90djsn801350d39fda',
+        'X-RapidAPI-Key': '5eee59729amsh14ba8d2e0c49119p1e060fjsn0283d7f8a93c',
         'X-RapidAPI-Host': 'ryanair.p.rapidapi.com'
       }
     };
